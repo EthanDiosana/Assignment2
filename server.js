4b2314c58fc81c7b3711c02eeb89e1bbb3fb5b4f
Liam Arabsky <liamarabsky@me.com>
2021-05-04 15:49:58 -0700

    server and client js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const port = 8080;
+
+app.get('/', (req, res) => {
+
+    fs.readFile('/index.html', (err, pgres) => {
+        if (err) {
+            res.writeHead(404);
+            res.write('Failed to locate resource.');
+        } else {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(pgres);
+        }
+        res.end();
+    });
+});
+
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+})
