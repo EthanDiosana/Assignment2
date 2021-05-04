dbae9fead02c937dacdb28c70e7f845d104d132a
Liam Arabsky <liamarabsky@me.com>
2021-05-04 16:23:32 -0700

    author credits

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
+const mysql = require('mysql');
+const db = mysql.createConnection({
+
+})
 const port = 8080;
 
 app.get('/', (req, res) => {
