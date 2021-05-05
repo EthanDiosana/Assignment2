92f18c791ea81bc575e251ab8ede449069554f78
John Braithwaite <jwbraith@gmail.com>
2021-05-05 13:54:55 -0700

    whipping up boilerplate ajax in the client.js

--- Scripts/client.js
+++ Scripts/client.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
+  function retrievePosts() {
+    $.ajax({
+      url: '/get-posts',
+      dataType: 'json',
+      type: 'GET',
+      success: function (data) {
+        //this should log all the rows from the posts table
+        console.log(data);
+        // then can use jquery to assign text to divs
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Error: ", jqXHR, textStatus, errorThrown);
+      }
+    });
+  }
+
+  $('#submit').on('click', function (event) {
+    event.preventDefault();
+
+    // encapsulate the data from the forms
+
+    $.ajax({
+      url: "/add-post",
+      dataType: "json",
+      type: "POST",
+      data: formData,
+      success: function (data) {
+        // log data to make sure the jquery above is properly grabbing it
+        console.log(data);
+
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Error: ", jqXHR, textStatus, errorThrown);
+      }
+    })
+  })
 
 });
